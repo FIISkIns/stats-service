7219085b7ce609eebfb7d5ff1e1ff1a12786a28f
Alexandru Pomohaci <alexandrupomohaci@yahoo.com>
2018-06-13 00:19:40 +0300

    Added health check, updated API

--- StatsServiceAPI.go
+++ StatsServiceAPI.go
@@ -43,8 +43,7 @@ type StatsItem struct {
 }
 
 func getCoursesProgress(userId string) (int, int, error) {
-	//CourseProgressService
-	resp, err := http.Get(config.CourseProgressServiceUrl + "/" + userId)
+	resp, err := http.Get(config.CourseProgressServiceUrl + "/progress/" + userId)
 	if err != nil {
 		return 0, 0, err
 	}
@@ -90,31 +89,23 @@ func getCoursesProgress(userId string) (int, int, error) {
 
 func getUserStats(userId string) (*StatsItem, error) {
 	var stats StatsItem
-
 	err := database.QueryRow("select currentStreak, longestStreak, lastLoggedIn, timeSpent from stats where userId = ?", userId).
 		Scan(&stats.CurrentStreak, &stats.LongestStreak, &stats.LastLoggedIn, &stats.TimeSpent)
-
 	if err != nil {
 		return nil, err
 	}
-
 	noOfStartedCourses, noOfCompletedCourses, err := getCoursesProgress(userId)
-
 	if err != nil {
 		return nil, err
 	}
-
 	stats.StartedCourses = noOfStartedCourses
 	stats.CompletedCourses = noOfCompletedCourses
-
 	return &stats, nil
 }
 
 func getStatsHandler(w http.ResponseWriter, _ *http.Request, ps httprouter.Params) {
-	err := database.Ping()
-	if err != nil {
-		http.Error(w, "Database error: unable to connect", 500)
-		log.Println(err)
+	if err := database.Ping(); err != nil {
+		http.Error(w, "Database error: unable to connect", http.StatusInternalServerError)
 		return
 	}
 	stats, err := getUserStats(ps.ByName("user"))
@@ -122,15 +113,14 @@ func getStatsHandler(w http.ResponseWriter, _ *http.Request, ps httprouter.Param
 		if err == sql.ErrNoRows {
 			http.Error(w, "User not found", 404)
 		} else {
-			http.Error(w, "Cannot retrieve user stats: "+err.Error(), 500)
+			http.Error(w, "Cannot retrieve user stats: "+err.Error(), http.StatusInternalServerError)
 		}
 
 		return
 	}
-
 	jsonData, err := json.Marshal(stats)
 	if err != nil {
-		http.Error(w, "JSON error: failed to marshal stats", 500)
+		http.Error(w, "JSON error: failed to marshal stats", http.StatusInternalServerError)
 		return
 	}
 	w.Header().Set("Content-Type", "application/json")
@@ -156,9 +146,8 @@ func isNextDay(firstDate, secondDate time.Time) bool {
 }
 
 func pingPostHandler(w http.ResponseWriter, _ *http.Request, ps httprouter.Params) {
-	err := database.Ping()
-	if err != nil {
-		http.Error(w, "Database error: unable to connect", 500)
+	if err := database.Ping(); err != nil {
+		http.Error(w, "Database error: unable to connect", http.StatusInternalServerError)
 		return
 	}
 	userId := ps.ByName("user")
@@ -168,23 +157,23 @@ func pingPostHandler(w http.ResponseWriter, _ *http.Request, ps httprouter.Param
 			stmt, err := database.Prepare("INSERT INTO stats(userId, longestStreak, currentStreak, lastLoggedIn , timeSpent)" +
 				" VALUES(?, ?, ?, ?, ?)")
 			if err != nil {
-				http.Error(w, "SQL error: failed to prepare insert statement", 500)
+				http.Error(w, "SQL error: failed to prepare insert statement", http.StatusInternalServerError)
 				return
 			}
 			_, err = stmt.Exec(userId, 1, 1, time.Now().UTC().Format(time.RFC3339), 0)
 			if err != nil {
-				http.Error(w, "Database error: failed to insert into stats", 500)
+				http.Error(w, "Database error: failed to insert into stats", http.StatusInternalServerError)
 				return
 			}
 		} else {
-			http.Error(w, "Cannot retrieve user stats: "+err.Error(), 500)
+			http.Error(w, "Cannot retrieve user stats: "+err.Error(), http.StatusInternalServerError)
 			return
 		}
 	} else {
 		currentTime := time.Now().UTC()
 		lastLoggedIn, err := time.Parse(time.RFC3339, stats.LastLoggedIn)
 		if err != nil {
-			http.Error(w, "Database error: lastLoggedIn wrong format\nNeeded format RFC3339", 500)
+			http.Error(w, "Database error: lastLoggedIn wrong format\nNeeded format RFC3339", http.StatusInternalServerError)
 			return
 		}
 		if isSameDay(lastLoggedIn, currentTime) {
@@ -207,12 +196,12 @@ func pingPostHandler(w http.ResponseWriter, _ *http.Request, ps httprouter.Param
 
 		stmt, err := database.Prepare("UPDATE stats SET currentStreak = ?, longestStreak = ?, lastLoggedIn = ?, timeSpent = ? where userId = ?")
 		if err != nil {
-			http.Error(w, "SQL error: cannot prepare update statement", 500)
+			http.Error(w, "SQL error: cannot prepare update statement", http.StatusInternalServerError)
 			return
 		}
 		_, err = stmt.Exec(stats.CurrentStreak, stats.LongestStreak, stats.LastLoggedIn, stats.TimeSpent, userId)
 		if err != nil {
-			http.Error(w, "Database error: failed to update stats", 500)
+			http.Error(w, "Database error: failed to update stats", http.StatusInternalServerError)
 			return
 		}
 	}
@@ -220,12 +209,11 @@ func pingPostHandler(w http.ResponseWriter, _ *http.Request, ps httprouter.Param
 }
 
 func initDatabase() error {
-	err := database.Ping()
-	if err != nil {
+	if err := database.Ping(); err != nil {
 		return err
 	}
 	var auxiliary int
-	err = database.QueryRow("SELECT userId FROM stats LIMIT 1").Scan(&auxiliary)
+	err := database.QueryRow("SELECT userId FROM stats LIMIT 1").Scan(&auxiliary)
 	if err != nil && err != sql.ErrNoRows {
 		stmt, err := database.Prepare("create table stats (" +
 			"userId int primary key," +
@@ -244,6 +232,34 @@ func initDatabase() error {
 	return nil
 }
 
+func checkHealth(w http.ResponseWriter, url string) bool {
+	resp, err := http.Get(url)
+	if err != nil {
+		http.Error(w, "Failed to communicate with: "+url+"\nCause: "+err.Error(), http.StatusInternalServerError)
+		return false
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		http.Error(w, "Failed to read response from: "+url+"\nCause: "+err.Error(), http.StatusInternalServerError)
+		return false
+	}
+	if resp.StatusCode != http.StatusOK {
+		http.Error(w, "Failed health check on: "+url+"\nResponse: "+string(body), http.StatusInternalServerError)
+		return false
+	}
+	return true
+}
+
+func healthCheckHandler(w http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
+	if err := database.Ping(); err != nil {
+		http.Error(w, "Database connection failed: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+	if success := checkHealth(w, config.CourseProgressServiceUrl+"/health"); !success {
+		return
+	}
+}
+
 func main() {
 	initConfig()
 	database, _ = sql.Open("mysql", config.DatabaseUrl)
@@ -252,10 +268,8 @@ func main() {
 	}
 	defer database.Close()
 	router := httprouter.New()
-	router.GET("/:user", getStatsHandler)
-	router.POST("/:user/ping", pingPostHandler)
-	err := http.ListenAndServe(":"+strconv.Itoa(config.Port), router)
-	if err != nil {
-		log.Fatal(err)
-	}
+	router.GET("/stats/:user", getStatsHandler)
+	router.POST("/stats/:user/ping", pingPostHandler)
+	router.GET("/health", healthCheckHandler)
+	log.Fatal(http.ListenAndServe(":"+strconv.Itoa(config.Port), router))
 }
